349d1cc25316d6efeea57e5f6611b64c1c8910b9
Andrew Diaz <andrewdiazn20@gmail.com>
2024-05-22 14:59:48 -0700

    first-commit

---
+++ src/Components/Home.js
@@ -0,0 +1,31 @@
+//useState: 
+//useEffect: when page loads we need to fetch food
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Banner from './Banner';
+
+function Home() {
+    const [foods, setFoods] = useState([])
+
+    useEffect(() => {
+        async function fetchData() {
+            const url = `${process.env.REACT_APP_BACKEND_URL}/Foods/all`
+            const response = await fetch(url)
+            const data = await response.json()
+            console.log(data)
+            console.log('yes')
+            setFoods(data)
+        }
+        fetchData()
+    }, [])
+
+
+
+    return (
+        <div>
+            <Banner />
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/Components/NewFood.js
@@ -0,0 +1,19 @@
+import { useState } from "react"
+function NewFood() {
+    const INITAL_STATE = {
+        name: '',
+        profilePicture: '',
+        type: '',
+        price: '',
+        description: '',
+        section: '',
+
+    }
+    return(
+        <div>
+            NewFood
+        </div>
+    )
+}
+
+export default NewFood
